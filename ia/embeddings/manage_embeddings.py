48ca82be482af4cdc1e322d94462277d4e2e284a
Walter Bivanco <walterbivanco@gmail.com>
2025-12-08 20:14:08 -0300

    Habilitar pysqlite3 para compatibilidad con Chroma en Azure

--- ia/embeddings/manage_embeddings.py
+++ ia/embeddings/manage_embeddings.py
@@ -1,6 +1,6 @@
-# __import__('pysqlite3')
-# import sys
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import pysqlite3
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
