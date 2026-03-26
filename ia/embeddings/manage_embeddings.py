3fa8ad0c8d9fed00a7e4b21ccc91c3424516cba0
Walter Bivanco <walterbivanco@gmail.com>
2026-03-26 19:46:21 -0300

    Se descomento para despliegue

--- ia/embeddings/manage_embeddings.py
+++ ia/embeddings/manage_embeddings.py
@@ -1,6 +1,6 @@
-# import pysqlite3
-# import sys
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import pysqlite3
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import os
 import shutil
